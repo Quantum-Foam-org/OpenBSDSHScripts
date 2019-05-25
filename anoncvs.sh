d37eb4ea90756133bc8cdd403af0a3ea0db5a0e1
Michael <maaj@laptop.Cisco>
2019-05-25 11:38:44 -0400

    use command instead of passthru and update paramter syntax

--- anoncvs.sh
+++ anoncvs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 openbsdVersion='OPENBSD_6_5'
-cvsup="cvs -d anoncvs@anoncvs4.usa.openbsd.org:/cvs  -q up -Pd -r$openbsdVersion"
+cvsServer='anoncvs@anoncvs4.usa.openbsd.org:/cvs'
+cvsup="cvs -d ${cvsServer} -q up -Pd -r${openbsdVersion}"
 cd /usr/ports
-echo $cvsup
+echo $(${cvsup})
 #cd /usr/src
 #echo `$cvsup`
 #cd /usr/xenocara
